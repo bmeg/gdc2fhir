dcae3b8f8b72604911c3e4e67c95860684e012c2
teslajoy <nasim@plenary.org>
2024-05-09 09:23:37 -0700

    pass fixture

--- tests/unit/test_cellosaurus_resource.py
+++ tests/unit/test_cellosaurus_resource.py
@@ -9,14 +9,11 @@ def cells_ndjson():
     return "./tests/fixtures/cellosaurus/cellosaurus_cellines.ndjson"
 
 
-def test_celline_resource():
+def test_celline_resource(cells_ndjson):
     cells_dir = "./tests/fixtures/cellosaurus/cells/"
     out_dir = "./tests/fixtures/cellosaurus/"
 
     cls = utils.cellosaurus_cancer_jsons(cells_dir)
     entity2fhir.fhir_ndjson(cls, os.path.join(out_dir, "cellosaurus_cellines_test.ndjson"))
 
-    assert filecmp.cmp("./tests/fixtures/cellosaurus/cellosaurus_cellines.ndjson",
-                       "./tests/fixtures/cellosaurus/cellosaurus_cellines_test.ndjson", shallow=False)
-
-
+    assert filecmp.cmp(cells_ndjson, "./tests/fixtures/cellosaurus/cellosaurus_cellines_test.ndjson", shallow=False)
