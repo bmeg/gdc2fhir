b7c04763f715d06d9f6f14b38b9c6a59784b67e3
teslajoy <nasim@plenary.org>
2024-05-31 16:29:17 -0700

    file convert test

--- tests/integration/test_convert.py
+++ tests/integration/test_convert.py
@@ -13,6 +13,11 @@ def cases():
     return utils.load_ndjson("./tests/fixtures/case/cases.ndjson")
 
 
+@pytest.fixture
+def files():
+    return utils.load_ndjson("./tests/fixtures/file/files.ndjson")
+
+
 def test_project_key_convert(projects):
     mapping.convert_maps(name="project", in_path="./tests/fixtures/project/projects.ndjson",
                          out_path="./tests/fixtures/project/project_key_test.ndjson", verbose=True)
@@ -27,3 +32,12 @@ def test_case_key_convert(cases):
 
     assert filecmp.cmp("./tests/fixtures/case/case_key_test.ndjson", "./tests/fixtures/case/case_key.ndjson", shallow=False)
     assert cases == utils.load_ndjson("./tests/fixtures/case/cases.ndjson")
+
+
+def test_document_reference_key_convert(files):
+    mapping.convert_maps(name="file", in_path="./tests/fixtures/file/files.ndjson",
+                         out_path="./tests/fixtures/file/file_key_test.ndjson", verbose=True)
+
+    assert filecmp.cmp("./tests/fixtures/file/file_key_test.ndjson", "./tests/fixtures/file/file_key.ndjson", shallow=False)
+    assert files == utils.load_ndjson("./tests/fixtures/file/files.ndjson")
+
