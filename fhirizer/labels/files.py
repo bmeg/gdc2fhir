37bbe01b54db5d8d7090d5e3be31662efa89bb2c
teslajoy <nasim@plenary.org>
2024-07-18 12:20:37 -0700

    DocRef observations

--- fhirizer/labels/files.py
+++ fhirizer/labels/files.py
@@ -152,8 +152,430 @@ file_maps = [
         destination=Destination(
             name='DocumentReference.date',
         )
-    )
+    ),
 
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.adapter_name',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.adapter_name',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.adapter_sequence',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.adapter_sequence',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.base_caller_name',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.base_caller_name',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.base_caller_version',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.base_caller_version',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.created_datetime',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.created_datetime',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.experiment_name',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.experiment_name',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.flow_cell_barcode',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.flow_cell_barcode',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.includes_spike_ins',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.includes_spike_ins',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.instrument_model',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.instrument_model',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.is_paired_end',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.is_paired_end',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.library_name',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.library_name',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.library_preparation_kit_catalog_number',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.library_preparation_kit_catalog_number',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.library_preparation_kit_name',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.library_preparation_kit_name',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.library_preparation_kit_vendor',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.library_preparation_kit_vendor',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.library_preparation_kit_version',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.library_preparation_kit_version',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.library_selection',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.library_selection',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.library_strand',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.library_strand',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.library_strategy',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.library_strategy',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.platform',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.platform',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.read_group_id',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.read_group_id',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.read_group_name',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.read_group_name',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.read_length',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.read_length',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.RIN',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.RIN',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.sequencing_center',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.sequencing_center',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.sequencing_date',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.sequencing_date',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.size_selection_range',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.size_selection_range',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.spike_ins_concentration',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.spike_ins_concentration',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.spike_ins_fasta',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.spike_ins_fasta',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.state',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.state',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.submitter_id',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.submitter_id',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.target_capture_kit_catalog_number',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.target_capture_kit_catalog_number',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.target_capture_kit_name',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.target_capture_kit_name',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.target_capture_kit_target_region',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.target_capture_kit_target_region',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.target_capture_kit_vendor',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.target_capture_kit_vendor',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.target_capture_kit_version',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.target_capture_kit_version',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.to_trim_adapter_sequence',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.to_trim_adapter_sequence',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.updated_datetime',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.updated_datetime',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.days_to_sequencing',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.days_to_sequencing',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.single_cell_library',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.single_cell_library',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.multiplex_barcode',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.multiplex_barcode',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.target_capture_kit',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.target_capture_kit',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.lane_number',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.lane_number',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.fragment_standard_deviation_length',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.fragment_standard_deviation_length',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.fragment_minimum_length',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.fragment_minimum_length',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.number_expect_cells',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.number_expect_cells',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.fragment_mean_length',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.fragment_mean_length',
+        )
+    ),
+
+    Map(
+        source=Source(
+            name='analysis.metadata.read_groups.fragment_maximum_length',
+        ),
+        destination=Destination(
+            name='Observation.DocumentReference.fragment_maximum_length',
+        )
+    )
 ]
 
 # out_path = os.path.join(package_dir, 'mapping', 'case.json')
