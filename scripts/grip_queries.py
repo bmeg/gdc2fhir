bcf44d252e90e5b896e6a641f9e17fc7476aa6a8
teslajoy <nasim@plenary.org>
2024-09-12 10:46:32 -0700

    docref observations

--- scripts/grip_queries.py
+++ scripts/grip_queries.py
@@ -4,7 +4,7 @@ import sys
 import gripql
 
 conn = gripql.Connection("http://localhost:8201")
-G=conn.graph("aced")
+G = conn.graph("aced")
 
 
 # Patient observations - test  --------------------
@@ -12,17 +12,29 @@ G.query().V().hasLabel("Patient").as_("patient").in_("focus_Patient").unwind("$.
 patient_observation = G.query().V().hasLabel("Patient").as_("patient").in_("focus_Patient").unwind("$.code.coding").pivot("$patient._gid", "$.code.coding.system", "$.code.coding.display").execute()
 patient_observation_df = pd.DataFrame(patient_observation)
 
+
+def edge_set(graph, direction) -> set:
+    """Fetches set of edges to graph node/vertex based on direction in/out"""
+    edges = []
+    edge_set = None
+
+    if direction in "out":
+        for row in graph.outE().execute():
+            edges.append(row['label'])
+        edge_set = set(edges)
+    elif direction in "in":
+        for row in graph.inE().execute():
+            edges.append(row['label'])
+        edge_set = set(edges)
+
+    if edge_set:
+        return edge_set
+
 # list the outgoing edges ---------------------------
-outgoing_edges = []
-for row in G.query().V().hasLabel('Patient').outE().execute():
-    outgoing_edges.append(row['label'])
-set(outgoing_edges)
+patient_out_edges = edge_set(graph=G.query().V().hasLabel('Patient'), direction="out")
 
 # list the incoming edges ---------------------------
-incoming_edges = []
-for row in G.query().V().hasLabel('Patient').inE().execute():
-    incoming_edges.append(row['label'])
-set(incoming_edges)
+patient_in_edges = edge_set(graph=G.query().V().hasLabel('Patient'), direction="in")
 
 
 # Patient - demographics ------------------------------
@@ -68,5 +80,27 @@ primary_diagnosis_df = pd.DataFrame(primary_diagnosis)
 primary_site = G.query().V().hasLabel("Patient").as_("patient").out("body_structure").unwind("$.includedStructure").unwind("$.includedStructure.structure.coding").pivot("$patient._gid", "$.includedStructure.structure.coding.system", "$.includedStructure.structure.coding.display").execute()
 primary_site_df = pd.DataFrame(primary_site)
 
-# Patient cancer stage TODO: seperate each type
-stage = G.query().V().hasLabel("Patient").as_("patient").out("condition").unwind("$.stage").unwind("$.stage.summary.coding").pivot("$patient._gid", "$.stage.summary.coding.system", "$.stage.summary.coding.display").execute()
+# Patient cancer stage TODO: Need to fetch from observation to create one column per stage classification
+stage = G.query().V().hasLabel("Patient").as_("patient").out("condition").unwind("$.stage").unwind("$.stage.type.coding").has(gripql.eq("$.stage.type.coding.system" ,"https://cadsr.cancer.gov/")).unwind("$.stage.summary.coding").has(gripql.eq("$.stage.summary.coding.system", "https://ncit.nci.nih.gov")).pivot("$patient._gid", "$.stage.type.coding.display", "$.stage.summary.coding.display").execute()
+stage_df = pd.DataFrame(stage)
+
+# Document Reference ---------------------
+file = G.query().V().hasLabel("DocumentReference").execute()
+file_type = G.query().V().hasLabel("Patient").as_("patient").out("document_reference").unwind("$.type.coding").pivot("$patient._gid", "$.type.coding.display", "$.type.coding.display").execute()
+file_type_df = pd.DataFrame(file_type)
+
+# list the outgoing edges ---------------------------
+file_out_edges = edge_set(graph=G.query().V().hasLabel("DocumentReference"), direction="out")
+
+# list the incoming edges ---------------------------
+file_in_edges = edge_set(graph=G.query().V().hasLabel("DocumentReference"), direction="in")
+
+# seperate value[x] boolean, string, integer, quantity, dateTime file observation components
+# in this case the df complement each-other. overlap will fill columns that are missing all cells if value[x] exists 
+file_component_valuestring = G.query().V().hasLabel("DocumentReference").as_("document").in_("focus_DocumentReference").unwind("$.component").unwind("$.component.code.coding").pivot("$document._gid", "$.component.code.coding.code", "$.component.valueString").execute()
+file_component_valuequantity = G.query().V().hasLabel("DocumentReference").as_("document").in_("focus_DocumentReference").unwind("$.component").unwind("$.component.code.coding").pivot("$document._gid", "$.component.code.coding.code", "$.component.valueQuantity").execute()
+# file_component_valueinteger = G.query().V().hasLabel("DocumentReference").as_("document").in_("focus_DocumentReference").unwind("$.component").unwind("$.component.code.coding").pivot("$document._gid", "$.component.code.coding.code", "$.component.valueInteger").execute()
+# file_component_valuedatetime = G.query().V().hasLabel("DocumentReference").as_("document").in_("focus_DocumentReference").unwind("$.component").unwind("$.component.code.coding").pivot("$document._gid", "$.component.code.coding.code", "$.component.valueDateTime").execute()
+
+file_component_valuestring_df = pd.DataFrame(file_component_valuestring)
+file_component_valuequantity_df = pd.DataFrame(file_component_valuequantity)
